0a9a10cfd8782137883abba8c166841ea0f40302
Supersant271 <santiagoabad.clementearredondo.gen23@cbtis105.edu.mx>
2025-05-12 08:45:08 -0600

    proyecto inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConsultasVuelos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
